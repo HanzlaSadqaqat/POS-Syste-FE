8f86026b431cb3a0f4c0e8ee7b0bee9d6c586f6c
Hanzla Sadaqat <hanzlasadaqatrajput@gmail.com>
2024-03-04 03:27:23 +0500

    User Dashboard almost completed

--- src/pages/User/HomePage/OrdersProcess.jsx
+++ src/pages/User/HomePage/OrdersProcess.jsx
@@ -19,27 +19,64 @@ import {
   Image,
 } from "@nextui-org/react";
 import { Button } from "antd";
+import axios from "axios";
 
 export default function OrdersProcess(props) {
   const [products, setProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
   const [selectId, setSelectId] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
   const [total, setTotal] = useState("0");
+  const [disableKey, setDisableKey] = useState([]);
+  const [max, setMax] = useState();
+  useEffect(() => {
+    getAllProducts();
+  }, []);
   useEffect(() => {
     if (props.products) {
+      const disable = props.products.filter((item) => {
+        if (item.quantity <= 0) {
+          return item._id;
+        }
+      });
+      setDisableKey(disable.map((item) => item._id));
       setProducts(props.products);
     } else {
       setProducts([]);
     }
   }, [props.products]);
+  const getAllProducts = async () => {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const response = await axios.get("/product", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log(response.data);
+      setAllProducts(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
-    const data = products.filter((item) => selectId.includes(item._id));
+    console.log("selectId", selectId);
+
+    const data = selectId.map((id) => {
+      const selectedItem = allProducts.find((item) => item._id === id);
+      return {
+        _id: selectedItem._id,
+        name: selectedItem.name,
+        price: selectedItem.price,
+        quantity: 1,
+      };
+    });
+
     setSelectedItems(data);
   }, [selectId]);
   useEffect(() => {
     const price = selectedItems.reduce((acc, cur) => {
-      console.log(acc.price, cur.price);
-      return acc + cur.price;
+      return acc + cur.price * cur.quantity;
     }, 0);
     console.log(price);
 
@@ -47,13 +84,50 @@ export default function OrdersProcess(props) {
   }, [selectedItems]);
 
   const handleQuantityChange = (id, val) => {
-    console.log(val);
-    console.log(id);
-    console.log(selectedItems);
-    const items = selectedItems.map((item, index) => {
+    let items = selectedItems.map((item) => {
+      // const findMax= max.find
       if (item._id === id) {
+        item.quantity = val;
       }
+      return item;
     });
+    console.log(items);
+
+    setSelectedItems(items);
+  };
+
+  const handleCreateOrder = async () => {
+    const finalItem = selectedItems.map((item) => {
+      const findItem = allProducts.find((ele) => ele._id === item._id);
+      console.log(findItem);
+      if (findItem.quantity < item.quantity) {
+        alert(`${findItem.name} is only ${findItem.quantity} remains`);
+      }
+      return {
+        productId: item._id,
+        name: item.name,
+        price: item.price * item.quantity,
+        quantity: item.quantity,
+      };
+    });
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const response = await axios.post(
+        "/order",
+        {
+          totalPrice: total,
+          orders: [...finalItem],
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -65,6 +139,7 @@ export default function OrdersProcess(props) {
             <Table
               color={"primary"}
               radius={"none"}
+              disabledKeys={disableKey}
               selectionMode="multiple"
               // defaultSelectedKeys={["2", "3"]}
               onSelectionChange={(e) => setSelectId([...e])}
@@ -83,7 +158,9 @@ export default function OrdersProcess(props) {
                     <TableCell>$ {item.price}</TableCell>
                     <TableCell>{item.quantity}</TableCell>
                     <TableCell>
-                      <Chip>{item.quantity > 0 ? "Active" : "Inactive"}</Chip>
+                      <Chip>
+                        {item.quantity > 0 ? "Active" : "Out of Stock"}
+                      </Chip>
                     </TableCell>
                   </TableRow>
                 ))}
@@ -100,7 +177,10 @@ export default function OrdersProcess(props) {
                 <CardHeader className="flex gap-3" radius="sm">
                   <div className="flex justify-between w-full items-center">
                     <p className="text-[32px]">Create Order</p>
-                    <Button className="bg-blue-500 text-white font-bold h-14">
+                    <Button
+                      className="bg-blue-500 text-white font-bold h-14"
+                      onClick={handleCreateOrder}
+                    >
                       Create
                     </Button>
                   </div>
@@ -117,33 +197,27 @@ export default function OrdersProcess(props) {
                       <TableColumn>Price</TableColumn>
                     </TableHeader>
                     <TableBody>
-                      {selectedItems.map((item) => (
+                      {selectedItems.map((item, index) => (
                         <TableRow key={item._id}>
                           <TableCell>{item.name}</TableCell>
                           <TableCell className="w-28">
                             <Input
                               type="number"
                               placeholder="0"
-                              defaultValue={1}
+                              defaultValue={item.quantity}
                               labelPlacement="outside"
-                              style={
-                                {
-                                  // width: "20px",
-                                }
-                              }
                               onChange={(e) =>
-                                handleQuantityChange(item._id, e.target.value)
+                                handleQuantityChange(
+                                  item._id,
+                                  parseInt(e.target.value)
+                                )
                               }
                               startContent={
-                                <div className="pointer-events-none flex items-center">
-                                  {/* <span className="text-default-400 text-small">
-                                    $
-                                  </span> */}
-                                </div>
+                                <div className="pointer-events-none flex items-center"></div>
                               }
                             />
                           </TableCell>
-                          <TableCell>{item.price}</TableCell>
+                          <TableCell>{item.price * item.quantity}</TableCell>
                         </TableRow>
                       ))}
                     </TableBody>
